1de97053c16cdb8466bd8ef0b3d795b78b352970
Juan Enrique Munoz <juanique@gmail.com>
2011-12-15 17:05:36 -0300

    added submodule tastypie

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,19 @@
-import settings
+from fabric.api import run
+import project.settings as settings
 
 def info():
     if settings.DEBUG:
         print "Server is in development mode."
     else:
         print "Server is in production mode."
+
+def init_submodules():
+    run("git submodule init")
+    run("git submodule update")
+
+def install():
+    try:
+        import tastypie
+    except ImportError:
+        pass
+
